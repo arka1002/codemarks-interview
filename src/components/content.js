61d2823c1b7d7298e9bab95ee77d80af939eca5f
arka1002 <arka.mcu700@gmail.com>
2023-02-01 00:04:33 +0530

    all for today

--- src/components/content.js
+++ src/components/content.js
@@ -1,4 +1,30 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { API } from "aws-amplify";
+import { listRevenues } from "../graphql/queries";
+
+
 export default function Content() {
+    // Access the client
+    const queryClient = useQueryClient();
+
+    const { isLoading, isError, data: revenue, error } = useQuery({
+        queryKey: ['revenue'],
+        queryFn: async () => {
+            const revenueData = await API.graphql({
+                query: listRevenues,
+            })
+            const revenues = revenueData.data.listRevenues.items;
+            return revenues;
+        }
+    })
+
+    if (isLoading) {
+        return <span>Loading...</span>
+    }
+
+    if (isError) {
+        return <span>Error: {error.message}</span>
+    }
     return (
         <>
             <p>Hi</p>
