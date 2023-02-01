6643d6f23c42409b3b1eea4a35dc0c2d1d298dd7
arka1002 <arka.mcu700@gmail.com>
2023-02-01 10:51:52 +0530

    added the form

--- src/components/content.js
+++ src/components/content.js
@@ -9,6 +9,9 @@ export default function Content() {
     // Access the client
     const queryClient = useQueryClient();
 
+    //react-hook-form
+    const { register, handleSubmit, reset, formState, formState: { errors }, formState: { isSubmitSuccessful } } = useForm();
+
     const { isLoading, isError, data: revenue, error } = useQuery({
         queryKey: ['revenue'],
         queryFn: async () => {
@@ -29,7 +32,7 @@ export default function Content() {
     }
 
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-y-4">
             <div>
                 <ul className="list-disc">
                     {
@@ -39,7 +42,45 @@ export default function Content() {
                     }
                 </ul>
             </div>
-            <div>Hi Mom</div>
+            <div>
+                <form className="flex flex-col gap-y-2" onSubmit={handleSubmit((data) => { console.log(data); })}>
+                    <div>
+                        <label for="year-choice">Choose year: </label>
+                        <input
+                            list="year-options"
+                            id="year-choice"
+                            name="year-choice" required
+                            className="border-2 border-indigo-600"
+                            {...register("year", { required: true })} />
+
+                        <datalist id="year-options">
+                            <option value="1998" />
+                            <option value="2008" />
+                            <option value="2018" />
+                            <option value="2028" />
+                            <option value="2038" />
+                        </datalist>
+                        <p>{errors.year && <span className='text-rose-500'>This field is required</span>}</p>
+                    </div>
+                    <div>
+                        <label for="revenue">Revenue : ₹ </label>
+
+                        <input
+                            type="text"
+                            id="revenue"
+                            name="revenue" required
+                            minLength="4"
+                            maxLength="8"
+                            size="10"
+                            className="border-2 border-indigo-600"
+                            {...register("revenue", { required: true })} />
+                        <p>{errors.revenue && <span className='text-rose-500'>This field is required</span>}</p>
+                    </div>
+                    <div>
+                        <button type="submit" className='transition ease-in-out delay-50 mt-5 border-solid border-2 border-indigo-600 px-2 py-2 rounded-md font-bold hover:bg-sky-500/50'>Add Bills</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
