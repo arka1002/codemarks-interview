9d5a6422e6efe808370be2fa70ad3ac8d2359696
arka1002 <arka.mcu700@gmail.com>
2023-02-01 18:44:01 +0530

    now...?

--- src/App.js
+++ src/App.js
@@ -6,24 +6,15 @@ import { useState } from "react";
 import { Flex, Divider } from "@aws-amplify/ui-react";
 
 function App() {
-  //The sidebar logic here
-  const [sidebarOpen, setSideBarOpen] = useState(false);
-  const handleViewSidebar = () => {
-    setSideBarOpen(!sidebarOpen);
-  };
-
+  
   return (
     <div className="App">
-      <p>
-        <button className="transition ease-in-out delay-50 border-solid border-2 border-indigo-600 px-4 py-4 rounded-md font-bold hover:bg-sky-500/50" onClick={handleViewSidebar}>&#9776;</button>
-      </p>
+      
       <Flex direction="column">
         <Divider orientation="horizontal" />
       </Flex>
-      <Sidebar isOpen={sidebarOpen} toggleSidebar={handleViewSidebar} />
-      <div className="flex justify-center">
-        <Content />
-      </div>
+      <Sidebar/>
+      
 
     </div>
   );
