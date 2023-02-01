ce5ad4c7dfbf90ee047c7334bead3fe84719ac8e
arka1002 <arka.mcu700@gmail.com>
2023-02-01 12:44:41 +0530

    DONE the MVP

--- src/components/indieEvents.js
+++ src/components/indieEvents.js
@@ -1,7 +1,23 @@
+import { deleteRevenue } from "../graphql/mutations";
+import { API } from "aws-amplify";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
 export default function IndieEvents({ id, year, revenue }) {
+    // Access the client
+    const queryClient = useQueryClient();
+    //delete bills
+    const deleteMutation = useMutation({
+        mutationFn: async (deletey) => await API.graphql({
+            query: deleteRevenue,
+            variables: { input: deletey }
+        }),
+        onSuccess: () => queryClient.invalidateQueries({ queryKey: ['revenue'] }),
+    })
     return (
         <>
-            <span className="text-amber-600 italic">Year :- </span>{year}, <span className="text-amber-600 italic">Revenue :- </span>₹{revenue}
+            <span className="text-amber-600 italic">Year :- </span>{year}, <span className="text-amber-600 italic">Revenue :- </span>₹{revenue}            <a className="italic cursor-pointer underline underline-offset-2 text-red-500" onClick={() => {
+                deleteMutation.mutate({ id: id })
+            }}>Delete &#9746;</a>
         </>
     );
 };
