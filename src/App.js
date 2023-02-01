10e132e33fbf9760c08f4260fecc3b68d15ca669
arka1002 <arka.mcu700@gmail.com>
2023-02-01 14:48:12 +0530

    will format the sidebar now

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="App">
       <p>
-        <button className="p-4 border-2 border-indigo-600" onClick={handleViewSidebar}>&#9776;</button>
+        <button className="transition ease-in-out delay-50 border-solid border-2 border-indigo-600 px-4 py-4 rounded-md font-bold hover:bg-sky-500/50" onClick={handleViewSidebar}>&#9776;</button>
       </p>
       <Flex direction="column">
         <Divider orientation="horizontal" />
